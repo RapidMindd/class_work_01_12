c674b40e91257f50eaa12e6a8f58799af0a24c40
RapidMindd <YaroslavT007@yandex.ru>
2025-12-01 16:59:49 +0300

    IDraw

--- main.cpp
+++ main.cpp
@@ -1,4 +1,41 @@
+#include <iostream>
+
+namespace top
+{
+  struct p_t
+  {
+    int x, y;
+  };
+
+  bool operator!=(p_t a, p_t b)
+  {
+    return a.x != b.x || a.y || b.y;
+  }
+
+  bool operator==(p_t a, p_t b)
+  {
+    return a.x == b.x && a.y == b.y;
+  }
+
+  struct IDraw
+  {
+    virtual p_t begin() = 0;
+    virtual p_t next(p_t) = 0;
+  };
+
+  size_t count(IDraw & d)
+  {
+    size_t k = 0;
+    p_t p = d.begin();
+    while(d.next(p) == d.begin())
+    {
+      k++;
+      p = d.next(p);
+    }
+  }
+}
+
 int main()
 {
-  
+  using namespace top;
 }
