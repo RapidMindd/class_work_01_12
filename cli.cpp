d26900a7d98b5e4548542686f396a5a16e41bfed
RapidMindd <YaroslavT007@yandex.ru>
2025-12-25 15:23:56 +0300

    design

--- cli.cpp
+++ cli.cpp
@@ -5,19 +5,37 @@ void hi()
   std::cout << "HI!\n";
 }
 
+std::istream& getword(std::istream& is, char* word, size_t k, bool(*c)(char))
+{
+
+}
+
+bool is_space(char c)
+{
+  return std::isspace(c);
+}
+
+size_t match(const char* word, const char* const* words, size_t k)
+{
+
+}
+
 int main()
 {
   constexpr size_t cmds_count = 1;
   void(*cmds[1])() = {hi};
+  const char* const cmds_text[] = {"hi"};
+  constexpr size_t b_size = 255;
+  char word[b_size + 1] = {};
   size_t i = 0;
-  while (!(std::cin >> i).eof())
+  while (!(getword(std::cin, word, b_size, is_space).eof()))
   {
     if (std::cin.fail())
     {
       std::cerr << "INVALID COMMAND\n";
       return 1;
     }
-    else if (i < cmds_count)
+    else if (size_t i = match(word, cmds_text, cmds_count); i < cmds_count)
     {
       cmds[i]();
     }
